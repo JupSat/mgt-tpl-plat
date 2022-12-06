605fec1561537fae0d54a246abad0be48cd3c9d1
JupSat <jupsat@163.com>
2022-12-06 23:10:04 +0800

    feat: 项目初始化

---
+++ src/main/java/com/mgt/plat/MgtTplPlatApplication.java
@@ -0,0 +1,13 @@
+package com.mgt.plat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MgtTplPlatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MgtTplPlatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mgt/plat/MgtTplPlatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mgt.plat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MgtTplPlatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
