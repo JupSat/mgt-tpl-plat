ee30b0148abc9a2bbf754644e5066ce6933c4fd3
JupSat <jupsat@163.com>
2023-02-25 21:01:20 +0800

    feat：修改拦截配置

--- src/main/java/com/mgt/plat/config/jwt/InterceptorConfig.java
+++ src/main/java/com/mgt/plat/config/jwt/InterceptorConfig.java
@@ -3,6 +3,7 @@ package com.mgt.plat.config.jwt;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import java.util.ArrayList;
@@ -35,6 +36,22 @@ public class InterceptorConfig extends WebMvcConfigurationSupport {
         excludePath.add(purchaseRecordImport);
         excludePath.add(purchaseRecordExport);
 
-        registry.addInterceptor(tokenHandler).excludePathPatterns(excludePath);
+        excludePath.add("/swagger-resources/**");
+        excludePath.add("/webjars/**");
+        excludePath.add("/v2/**");
+        excludePath.add("/favicon.ico");
+        excludePath.add("/swagger-ui.html/**");
+        registry.addInterceptor(tokenHandler).addPathPatterns("/**").excludePathPatterns(excludePath);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations(
+                "classpath:/static/");
+        registry.addResourceHandler("/swagger-ui.html").addResourceLocations(
+                "classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations(
+                "classpath:/META-INF/resources/webjars/");
+        super.addResourceHandlers(registry);
     }
 }
